4e6aebde6c306a9bbb4aadb0f60d7c0e8e3dd58c
ETTARAK <ettarak.zouhair@gmail.com>
2020-12-09 17:26:48 +0100

    Coding, how to coding Methods and call them

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#app',
     data: {
         Title: 'Formation VueJS version 2'
+    },
+    methods:{
+        hello : function () {
+            return `Hello ${this.Title}`;
+        }
     }
 })
