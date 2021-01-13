dd0b735453ce0fccffc5bca066306e71f281bdc6
ETTARAK <ettarak.zouhair@gmail.com>
2021-01-13 15:08:16 +0100

    Coding, Event Click or DblClick

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             title: 'Formation VueJs '+ new Date().toLocaleString(),
-            author :  'Zouhair ET-TARAK'
+            author :  'Zouhair ET-TARAK',
+            version : 'vue 3',
+            count : 0
         }
     },
     methods: {
         hello() {
             return `Hello Mr. ${this.author}`;
+        },
+        increment(step) {
+            this.count +=step;
+        },
+        decrement(step) {
+            this.count -=step;
         }
     }
 });
