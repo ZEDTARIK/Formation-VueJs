1733f778c70a306f7f95568a127f0f7dbfd2a58b
ETTARAK <ettarak.zouhair@gmail.com>
2021-01-13 16:13:08 +0100

    Coding, watching chnage function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: 'Formation VueJs '+ new Date().toLocaleString(),
-            author :  'Zouhair ET-TARAK',
-            version : 'vue 3',
-            count : 0,
-            fullName : '',
-            initValue : 0 
+            title: 'Formation VueJs ' + new Date().toLocaleString(),
+            author: 'Zouhair ET-TARAK',
+            version: 'vue 3',
+            count: 1,
+            fullName: '',
+            initValue: 0,
+            message: ''
         }
     },
     methods: {
@@ -14,16 +15,24 @@ const app = Vue.createApp({
             return `Hello Mr. ${this.author}`;
         },
         increment(step) {
-            this.count +=step;
+            this.count += step;
         },
         decrement(step) {
-            this.count -=step;
+            this.count -= step;
         }
     },
-    computed : {
-            calcul : function() {
-                return this.initValue * this.count;
-            }
+    computed: {
+        calcul: function () {
+            return this.initValue * this.count;
+        }
+    },
+    watch: {
+        calcul: function (value) {
+            if (value >= 10)
+                this.message = 'WINNER';
+            else
+                this.message = 'LOSER';
+        }
     }
 });
 app.mount('#app');
